58ed2b954b5ffd0587178a7debee896e0fe427fa
Rio Swarawan Putra <swarawan.rio@gmail.com>
2018-05-04 11:34:21 +0700

    [RIO] add convert date extension

--- corelibrary/src/main/java/com/swarawan/corelibrary/extensions/CommonExt.kt
+++ corelibrary/src/main/java/com/swarawan/corelibrary/extensions/CommonExt.kt
@@ -3,12 +3,14 @@ package com.swarawan.corelibrary.extensions
 import android.util.Patterns
 import com.swarawan.corelibrary.utils.TextUtils
 import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 /**
  * Created by rioswarawan on 2/11/18.
  */
 
-inline fun <reified T: Any> clazz() = T::class.java
+inline fun <reified T : Any> clazz() = T::class.java
 
 fun String.isNumeric(): Boolean = matches("^[0-9]+\$".toRegex())
 
@@ -33,4 +35,15 @@ fun Long.convertToCurrency(countryCurrency: String = TextUtils.BLANK): String {
 fun Int.convertToCurrency(countryCurrency: String = TextUtils.BLANK): String {
     val value = NumberFormat.getNumberInstance().format(this).replace(",", ".")
     return "$countryCurrency $value"
+}
+
+fun String.convertDate(inputFormat: String = "yyyy-MM-dd", outputFormat: String = "dd MMMM yyyy"): String {
+    val dateFormat = SimpleDateFormat(inputFormat, Locale.getDefault())
+    val requiredFormat = SimpleDateFormat(outputFormat, Locale.getDefault())
+    try {
+        val date = dateFormat.parse(this)
+        return requiredFormat.format(date)
+    } catch (e: Exception) {
+    }
+    return TextUtils.BLANK
 }
